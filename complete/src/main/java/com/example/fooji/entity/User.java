aa68b6379d4d61dfec3c4d24c1acf9a456e0c761
Sergey <luksemburg.77@gmail.com>
2025-08-06 01:12:30 +0300

    User Entity added

---
+++ complete/src/main/java/com/example/fooji/entity/User.java
@@ -0,0 +1,143 @@
+package com.example.fooji.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "lastLogin_at")
+    private LocalDateTime lastLoginAt;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    private Long phone;
+
+    private String gender;
+
+    private String location;
+
+    @Column(name = "is_notify", nullable = false)
+    private Boolean isNotify;
+
+    // Constructors
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public LocalDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Boolean getNotify() {
+        return isNotify;
+    }
+
+    public void setNotify(Boolean notify) {
+        isNotify = notify;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public void setLastLoginAt(LocalDateTime lastLoginAt) {
+        this.lastLoginAt = lastLoginAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
